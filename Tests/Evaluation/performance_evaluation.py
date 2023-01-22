53f4ed7f0d78fe8ae60c874f85269aef77837388
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2023-01-22 17:16:32 +0000

    Performance Checking

---
+++ Tests/Evaluation/performance_evaluation.py
@@ -0,0 +1,70 @@
+import itertools
+import re
+import time
+import os
+import sys
+from math import comb
+working_dir = os.getcwd()
+os.chdir("../../..")
+sys.path.append(os.getcwd())
+os.chdir(working_dir)
+from runner import Runner
+from Internal_Representation.problem_predicate import ProblemPredicate
+from Solver.Parameter_Selection.ParameterSelector import ParameterSelector
+from Solver.Search_Queues.Greedy_Best_First_Search_Queue import GBFSSearchQueue
+from Solver.Heuristics.hamming_distance_partial_order import HammingDistancePartialOrder
+from Solver.Heuristics.tree_distance import TreeDistance
+
+
+def run_test(domain_file_path, problem_file_path):
+    print(domain_file_path)
+    print(problem_file_path)
+    controller = Runner(domain_file_path, problem_file_path)
+    controller.set_search_queue(GBFSSearchQueue)
+    controller.set_heuristic(HammingDistancePartialOrder)
+    controller.parse_domain()
+    controller.parse_problem()
+
+    # Start Search
+    controller.solver.solve(search=False)
+    res = None
+    start_time = time.time()
+    res = controller.solver._search()
+    end_time = time.time()
+    solve_time = end_time - start_time
+    models_created = res.model_counter
+
+    # Write to file
+    problem_file_path_slashes = [i.start() for i in re.finditer('/', problem_file_path)]
+    write_to_file(problem_file_path[problem_file_path_slashes[-2] + 1:], solve_time, models_created)
+
+
+def write_to_file(problem_name, solve_time, models_created):
+    file_name = 'performance-evaluation-results.csv'
+    if os.path.exists(file_name):
+        # If file exists open it and append
+        write_file = open(file_name, 'a')
+    else:
+        # If file does not exist make one
+        write_file = open(file_name, 'w')
+        write_file.write('Problem,solve time,models created')
+    write_file.write("\n{},{},{}".format(problem_name, solve_time, models_created))
+    write_file.close()
+
+
+if __name__ == "__main__":
+    """Rover Problems"""
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p01.hddl")
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p02.hddl")
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p03.hddl")
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p04.hddl")
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p05.hddl")
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p06.hddl")
+    run_test("../Examples/Rover/domain.hddl", "../Examples/Rover/p07.hddl")
+    """Barman Problems"""
+    run_test("../Examples/Barman/domain.hddl", "../Examples/Barman/pfile01.hddl")
+    """Depots Problems"""
+    run_test("../Examples/Depots/domain.hddl", "../Examples/Depots/p01.hddl")
+    """Factories Problems"""
+    run_test("../Examples/Factories/domain.hddl", "../Examples/Factories/pfile01.hddl")
+
