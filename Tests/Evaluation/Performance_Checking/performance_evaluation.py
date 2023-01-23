01f447a5f46192110a1ee823f789c7754ed929a0
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2023-01-23 20:43:06 +0000

    Added folder for performance checking

--- Tests/Evaluation/Performance_Checking/performance_evaluation.py
+++ Tests/Evaluation/Performance_Checking/performance_evaluation.py
@@ -17,14 +17,17 @@ from Solver.Heuristics.tree_distance import TreeDistance
 from Solver.Heuristics.no_pruning import NoPruning
 from Solver.Heuristics.pruning import Pruning
 from Solver.Heuristics.seen_states_pruning import SeenStatesPruning
+from Solver.Heuristics.hamming_distance_seen_states import HammingDistanceSeenStatesPruning
+from Solver.Heuristics.tree_distance_seen_states import TreeDistanceSeenStatesPruning
+from Solver.Heuristics.tree_distance import TreeDistance
 
 
 def run_test(domain_file_path, problem_file_path):
     print(domain_file_path)
     print(problem_file_path)
     controller = Runner(domain_file_path, problem_file_path)
-    # controller.set_search_queue(GBFSSearchQueue)
-    controller.set_heuristic(SeenStatesPruning)
+    controller.set_search_queue(GBFSSearchQueue)
+    controller.set_heuristic(TreeDistanceSeenStatesPruning)
     controller.parse_domain()
     controller.parse_problem()
 
