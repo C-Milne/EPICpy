0101f6059713c016bd1d985aa23534407c54aaf5
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2023-02-21 17:05:24 +0000

    Removed redundant code

--- Solver/Solving_Algorithms/partial_order_novelty.py
+++ Solver/Solving_Algorithms/partial_order_novelty.py
@@ -53,16 +53,13 @@ class PartialOrderNoveltySolver(PartialOrderSolver):
                 if type(eff) == Effects.Effect:
                     param_list = []
                     for i in eff.parameters:
-                        try:
-                            if i in subtask.given_params:
-                                param_list.append(subtask.given_params[i])
-                            else:
-                                # Check constants
-                                const = self.domain.get_constant(i)
-                                assert const is not None
-                                param_list.append(const)
-                        except Exception as e:
-                            raise NotImplementedError
+                        if i in subtask.given_params:
+                            param_list.append(subtask.given_params[i])
+                        else:
+                            # Check constants
+                            const = self.domain.get_constant(i)
+                            assert const is not None
+                            param_list.append(const)
 
                     if eff.negated:
                         # Predicate needs to be removed
