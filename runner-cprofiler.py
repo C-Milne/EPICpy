a928b7c1cdaf3f554d65f48f33a4126ca7d61e2f
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2023-03-22 18:01:39 +0000

    Further Delete Relaxed Optimisation

--- runner-cprofiler.py
+++ runner-cprofiler.py
@@ -87,7 +87,7 @@ result = '\n'.join([','.join(line.rstrip().split(None, 5)) for line in result.sp
 # output/runner-landmarks-profile5Rover5WithDeleteRelaxed7.csv
 # output/runner-tree-hamming_panda_verify6.csv
 # output/runner-tree-hamming-PO-light.csv
-with open('output/runner-landmarks-newest-barman2.csv', 'w+') as f:
+with open('output/runner-landmarks-newest-barman3.csv', 'w+') as f:
     # f=open(result.rsplit('.')[0]+'.csv','w')
     f.write(result)
     f.close()
