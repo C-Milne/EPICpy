2bba80bdb84cdb0f256c6e7b02ea18f67a082a4d
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2023-03-27 15:42:54 +0100

    Additions for novelty with methods

--- Solver/Solving_Algorithms/partial_order_novelty_methods.py
+++ Solver/Solving_Algorithms/partial_order_novelty_methods.py
@@ -6,19 +6,33 @@ class PartialOrderNoveltyMethodsSolver(PartialOrderNoveltySolver):
     def __init__(self, domain, problem):
         super().__init__(domain, problem)
         self._seen_methods = set()
+        self._num_novel_methods = 0
+        self._num_not_novel_methods = 0
+        self._num_novel_method_not_novel_state = 0
+        self._num_novel_methods_novel_state = 0
 
     def _add_model_to_search_queue(self, model, addition):
         """This is where models are added to the queue after expanding an abstract task or method.
         When decomposing a task, this resets to 0"""
-        novelty = self._check_method_novelty(addition)
+        novelty = self._check_addition_novelty(model, addition)
         self.search_models.novelty_add(model, novelty)
+
+    def _check_addition_novelty(self, model, addition):
+        novelty = 0
+        if type(addition.task) == Method:
+            novelty = self._check_method_novelty(addition)
+        return novelty
 
     def _check_method_novelty(self, addition):
         novelty = 0
-        if type(addition.task) == Method:
-            added_method = hash((addition.task, tuple(addition.given_params.values())))
-            initial_size = len(self._seen_methods)
-            self._seen_methods.add(added_method)
-            if initial_size < len(self._seen_methods):
-                novelty = 1
+        added_method = hash((addition.task, tuple(addition.given_params.values())))
+        initial_size = len(self._seen_methods)
+        self._seen_methods.add(added_method)
+        if initial_size < len(self._seen_methods):
+            novelty = 1
+
+        if novelty > 0:
+            self._num_novel_methods += 1
+        else:
+            self._num_not_novel_methods += 1
         return novelty
