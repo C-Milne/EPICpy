ac871704965796f7215dc010df053e88a4df77c5
Cael Milne <caelmilne2001@gmail.com>
2023-08-21 22:25:47 +0100

    Added problem unittest that needed to be implemented

--- Tests/test_problem.py
+++ Tests/test_problem.py
@@ -6,11 +6,12 @@ import sys
 
 current_dir = os.getcwd()
 if current_dir.endswith('Tests'):
-    os.chdir('')
+    os.chdir('..')
     sys.path.append(os.getcwd())
 
 from Internal_Representation.domain import Domain
 from Internal_Representation.problem import Problem
+from Internal_Representation.Object import Object
 
 
 class ProblemTests(unittest.TestCase):
@@ -19,5 +20,13 @@ class ProblemTests(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_add_object(self):
-        self.assertEqual(1, 2)
+    def test_add_list_of_objects(self):
+        domain = Domain(None)
+        problem = Problem(domain)
+        domain.add_problem(problem)
+        list_of_objects = [Object('MockOb1'), Object('MockOb2'), Object('MockOb3')]
+        problem.add_object(list_of_objects)
+
+        self.assertIn('MockOb1', problem.objects)
+        self.assertIn('MockOb2', problem.objects)
+        self.assertIn('MockOb3', problem.objects)
